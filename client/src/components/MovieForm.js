88d6d075473a7ff89329bc20265c5db8c6a19627
Megan McCarty <megan_mccarty@hotmail.com>
2021-08-11 12:09:16 -0400

    commit tree 0fbe33a9c460fa913e2f3553b42dbeec4d7df4ba

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -2,133 +2,149 @@ import { useState } from "react";
 import styled from "styled-components";
 
 function MovieForm() {
-  const [formData, setFormData] = useState({
-    title: "",
-    year: new Date().getFullYear(),
-    length: "0",
-    director: "",
-    description: "",
-    poster_url: "",
-    category: "",
-    discount: false,
-    female_director: false,
-  });
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    fetch("/movies", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    })
-      .then((response) => response.json())
-      .then((newMovie) => console.log(newMovie));
-  }
-
-  function handleChange(e) {
-    const value =
-      e.target.type === "checkbox" ? e.target.checked : e.target.value;
-    setFormData({
-      ...formData,
-      [e.target.id]: value,
+    const [errors, setErrors] = useState([]);
+    const [formData, setFormData] = useState({
+        title: "",
+        year: new Date().getFullYear(),
+        length: "0",
+        director: "",
+        description: "",
+        poster_url: "",
+        category: "",
+        discount: false,
+        female_director: false,
     });
-  }
 
-  return (
-    <Wrapper>
-      <form onSubmit={handleSubmit}>
-        <FormGroup>
-          <label htmlFor="title">Title</label>
-          <input
-            type="text"
-            id="title"
-            value={formData.title}
-            onChange={handleChange}
-          />
-        </FormGroup>
-        <FormGroup>
-          <label htmlFor="year">Year</label>
-          <input
-            type="number"
-            id="year"
-            min="1888"
-            max={new Date().getFullYear()}
-            value={formData.year}
-            onChange={handleChange}
-          />
-        </FormGroup>
-        <FormGroup>
-          <label htmlFor="length">Length</label>
-          <input
-            type="number"
-            id="length"
-            value={formData.length}
-            onChange={handleChange}
-          />
-        </FormGroup>
-        <FormGroup>
-          <label htmlFor="director">Director</label>
-          <input
-            type="text"
-            id="director"
-            value={formData.director}
-            onChange={handleChange}
-          />
-        </FormGroup>
-        <FormGroup>
-          <label htmlFor="description">Description</label>
-          <textarea
-            id="description"
-            value={formData.description}
-            onChange={handleChange}
-          />
-        </FormGroup>
-        <FormGroup>
-          <label htmlFor="poster_url">Poster</label>
-          <input
-            type="text"
-            id="poster_url"
-            value={formData.poster_url}
-            onChange={handleChange}
-          />
-        </FormGroup>
-        <FormGroup>
-          <label htmlFor="category">Category</label>
-          <input
-            type="text"
-            id="category"
-            value={formData.category}
-            onChange={handleChange}
-          />
-        </FormGroup>
-        <FormGroup>
-          <label htmlFor="discount">
-            Discount?
-            <input
-              type="checkbox"
-              id="discount"
-              checked={formData.discount}
-              onChange={handleChange}
-            />
-          </label>
-        </FormGroup>
-        <FormGroup>
-          <label htmlFor="female_director">
-            Female Director?
-            <input
-              type="checkbox"
-              id="female_director"
-              checked={formData.female_director}
-              onChange={handleChange}
-            />
-          </label>
-        </FormGroup>
-        <SubmitButton type="submit">Add Movie</SubmitButton>
-      </form>
-    </Wrapper>
-  );
+    async function handleSubmit(e) {
+        e.preventDefault();
+        // fetch returns a Promise, we must await it
+        const response = await fetch("/movies", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData),
+        });
+        // response.json() returns a Promise, we must await it
+        const data = await response.json();
+        if (response.ok) {
+            console.log("Movie created:", data);
+        } else {
+            setErrors(data.errors);
+        }
+    }
+
+    function handleChange(e) {
+        const value =
+            e.target.type === "checkbox" ? e.target.checked : e.target.value;
+        setFormData({
+            ...formData,
+            [e.target.id]: value,
+        });
+    }
+
+    return (
+        <Wrapper>
+            {
+                errors.length > 0 && (
+                    <ul style={{ color: "red" }}>
+                        {errors.map((error) => (
+                            <li key={error}>{error}</li>
+                        ))}
+                    </ul>
+                )
+            }
+            <form onSubmit={handleSubmit}>
+                <FormGroup>
+                    <label htmlFor="title">Title</label>
+                    <input
+                        type="text"
+                        id="title"
+                        value={formData.title}
+                        onChange={handleChange}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <label htmlFor="year">Year</label>
+                    <input
+                        type="number"
+                        id="year"
+                        min="1888"
+                        max={new Date().getFullYear()}
+                        value={formData.year}
+                        onChange={handleChange}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <label htmlFor="length">Length</label>
+                    <input
+                        type="number"
+                        id="length"
+                        value={formData.length}
+                        onChange={handleChange}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <label htmlFor="director">Director</label>
+                    <input
+                        type="text"
+                        id="director"
+                        value={formData.director}
+                        onChange={handleChange}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <label htmlFor="description">Description</label>
+                    <textarea
+                        id="description"
+                        value={formData.description}
+                        onChange={handleChange}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <label htmlFor="poster_url">Poster</label>
+                    <input
+                        type="text"
+                        id="poster_url"
+                        value={formData.poster_url}
+                        onChange={handleChange}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <label htmlFor="category">Category</label>
+                    <input
+                        type="text"
+                        id="category"
+                        value={formData.category}
+                        onChange={handleChange}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <label htmlFor="discount">
+                        Discount?
+                        <input
+                            type="checkbox"
+                            id="discount"
+                            checked={formData.discount}
+                            onChange={handleChange}
+                        />
+                    </label>
+                </FormGroup>
+                <FormGroup>
+                    <label htmlFor="female_director">
+                        Female Director?
+                        <input
+                            type="checkbox"
+                            id="female_director"
+                            checked={formData.female_director}
+                            onChange={handleChange}
+                        />
+                    </label>
+                </FormGroup>
+                <SubmitButton type="submit">Add Movie</SubmitButton>
+            </form>
+        </Wrapper>
+    );
 }
 
 const Wrapper = styled.section`
